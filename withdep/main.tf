70222d905dc64a00b895ebda6ebeae41eedeff5b
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-11-11 15:59:54 -0600

    Add a module with dependency

---
+++ withdep/main.tf
@@ -0,0 +1,19 @@
+module "eg_prod_bastion_label" {
+  source = "cloudposse/label/null"
+  version = "= 0.25.0"
+
+  namespace  = "eg"
+  stage      = "prod"
+  name       = "bastion"
+  attributes = ["public"]
+  delimiter  = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+    "Snapshot"     = "true"
+  }
+}
+
+output "tags" {
+  value = module.eg_prod_bastion_label.tags
+}
